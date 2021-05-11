a0036d393ca896d7dab921d695d9b78f85de92bb
unknown <sidchaturvedi.2000@gmail.com>
2021-05-11 23:48:31 +0530

    Drag n Drop

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Board (props) 
+{
+    const drop = event => {
+        event.preventDefault();
+        const card_id = event.dataTransfer.getData('card_id') //card_id is a key
+        const card = document.getElementById(card_id);
+        card.style.display = 'block';
+        event.target.appendChild(card);
+
+    }
+
+    const dragOver = event => {
+        event.preventDefault();
+    }
+
+    return(
+        <div 
+            id ={props.id}
+            onDrop = {drop}
+            onDragOver = {dragOver}
+            className = {props.className}
+        >
+            {props.children}
+        </div>
+    )
+
+}
+
+export default Board;
